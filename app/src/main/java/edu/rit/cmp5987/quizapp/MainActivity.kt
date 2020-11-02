c0927ee47f2296c6c1730597bc6e7329ae47548c
Catie Poggioli <cmp5987@rit.edu>
2020-11-01 20:59:28 -0500

    Initial Quiz App

---
+++ app/src/main/java/edu/rit/cmp5987/quizapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.rit.cmp5987.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
